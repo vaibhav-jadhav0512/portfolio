6fceab6aa676d7f579ccae555d322482085ff1b0
Vaibhav Jadhav <vaibhav-jadhav0512>
2023-05-12 03:06:12 +0530

    final commit

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -37,9 +37,9 @@ const Contact = () => {
         import.meta.env.VITE_APP_EMAILJS_TEMPLATE_ID,
         {
           from_name: form.name,
-          to_name: "JavaScript Mastery",
+          to_name: "Vaibhav Jadhav",
           from_email: form.email,
-          to_email: "sujata@jsmastery.pro",
+          to_email: "vaibhav.jadhav0596@gmail.com",
           message: form.message,
         },
         import.meta.env.VITE_APP_EMAILJS_PUBLIC_KEY
